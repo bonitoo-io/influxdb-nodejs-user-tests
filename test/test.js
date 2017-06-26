bbd20ee49d5c08312ef62436d888fd3b7def42cd
karel-rehor <karel.k.rehor@email.cz>
2017-06-26 17:52:06 +0200

    WIP adding type tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 let InfluxDB = require('influxdb-nodejs')
 let assert = require('assert')
 
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
+
 describe('InfluxDB.Connect', function(){
 
     describe('#Check connection', function(){
@@ -27,7 +31,7 @@ describe('InfluxDB.Connect', function(){
     describe('#Write datapoints', function(){
 
         it('should write datapoints to the server', function(done){
-             
+
 
               let connection = new InfluxDB.Connection({
 
@@ -123,17 +127,97 @@ describe('InfluxDB.Connect', function(){
                         console.log('DROP QUERY EXEC ERROR')
                         done(e)
                     })
-  
+
               }).catch((e) => {
                     console.log('error',e)
                     done(e)
               });
-
-
 
          })
 
     });
 
 
+});
+
+describe('InfluxDB.types', function(){
+
+  let connection = new InfluxDB.Connection({
+        database: 'test1'
+  })
+
+  connection.connect().then((result) => {
+
+      describe('#floats', function(){
+
+         it('should write floats in all formats', function(done){
+            let dPF1 = {
+               measurement: 'powerf',
+               timestamp: new Date().getTime() + 1000000,
+               tags: [{key: 'location', value: 'Turbine0017'}],
+               fields: [{key: 'kwatts', value: 49}]
+             };
+
+             let dPF2 = {
+                measurement: 'powerf',
+                timestamp: new Date().getTime() + 1000000,
+                tags: [{key: 'location', value: 'Turbine0018'}],
+                fields: [{key: 'kwatts', value: 49.013}]
+            };
+
+            let dPF3 = {
+               measurement: 'powerf',
+               timestamp: new Date().getTime() + 1000000,
+               tags: [{key: 'location', value: 'Turbine0019'}],
+               fields: [{key: 'kwatts', value: 5.009e+1}]
+            };
+
+            connection.write([dPF1, dPF2, dPF3]).then(() => {
+            }).catch((e) = {
+
+            });
+
+            connection.flush().then(() => {
+
+            }).catch((e) => {
+                console.log('ERROR ON FLUSH')
+                done(e);
+            });
+
+            //wait a sec for server to do its thing
+            sleep(1000).then(() => {
+
+               connection.executeQuery('select * from powerf').then((result) => {
+//                    console.log(result)
+                    for( dp in result){
+                      switch(result[dp].location){
+                            case 'Turbine0017': //49
+                                assert.equal(result[dp].kwatts, 49.0)
+                                break;
+                            case 'Turbine0018': //49.013
+                                assert.equal(result[dp].kwatts, 49.013)
+                                break;
+                            case 'Turbine0019': //5.009e+1
+                                assert.equal(result[dp].kwatts, 50.09)
+                                break;
+                            default:
+                                assert.fail(result[dp].kwatts,result[dp].location,
+                                        'unexpected element in results array', ',')
+                                break;
+                      }
+                    }
+                    done();
+               }).catch((e) => {
+                    console.log('ERROR ON READ BACK')
+                    done(e)
+               });
+
+             });
+
+         });
+
+      });
+  }).catch((e) => {
+       console.log('error', e)
+  });
 });
